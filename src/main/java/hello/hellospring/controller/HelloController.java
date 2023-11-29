0468ec3580e8090af3760259a8cd013291d3054f
Sunny14578 <imsun0101@naver.com>
2023-11-29 22:12:57 +0900

    [fix]:오류 수정

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -10,6 +10,7 @@ public class HelloController {
     @GetMapping("hello")
     public String hello(Model model){
         model.addAttribute("data", "hello!!");
+//        흠
         return "hello";
     }
 
