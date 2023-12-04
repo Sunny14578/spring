7ab94797e450005af01f77d0c509a2e22adc8d8e
Sunny14578 <imsun0101@naver.com>
2023-12-04 22:02:27 +0900

    [edit]:코멘트

--- src/main/java/hello/hellospring/aop/TimeTraceAop.java
+++ src/main/java/hello/hellospring/aop/TimeTraceAop.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class TimeTraceAop {
-//    ...
+//    ......
     @Around("execution(* hello.hellospring..*(..))")
     public Object execut(ProceedingJoinPoint joinPoint) throws Throwable{
         long start = System.currentTimeMillis();
